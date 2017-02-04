f9ac6e0090077634cba94593b4395b82edc6c404
Scott Lowrey <sdlowrey@gmail.com>
2017-02-04 15:46:18 -0500

    multiple service fan-in - seems to run in lockstep even with rand?

--- boring.go
+++ boring.go
@@ -19,12 +19,17 @@ func boring(msg string) chan string {
 	return c
 }
 
+func fanIn(in1, in2 <-chan string) chan string{
+	c := make(chan string)
+	go func() { for { c <- <-in1}}()
+	go func() { for { c <- <-in2}}()
+	return c
+}
+
 func main() {
-	donnie := boring("blah blah")
-	walter := boring("shut the fuck up")
-	for  i := 0; i < 5; i++{
-		fmt.Println(<- donnie)
-		fmt.Println(<- walter)
+	c := fanIn(boring("blah blah"), boring("shut up"))
+	for  i := 0; i < 10; i++{
+		fmt.Println(<- c)
 	}
 	fmt.Println("You're boring. Bye!")
 }
