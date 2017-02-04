f4d73161801da285d7c9814a6aab479fb511a301
Scott Lowrey <sdlowrey@gmail.com>
2017-02-04 15:47:39 -0500

    fan-in without delay (more random)

--- boring.go
+++ boring.go
@@ -2,17 +2,13 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"time"
 )
 
 func boring(msg string) chan string {
 	c := make(chan string)
-	rand.Seed(rand.Int63())
 	go func() {
 		for i := 0; ; i++ {
 			c <- fmt.Sprintf("%s %d", msg, i)
-			time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
 		}
 		close(c)
 	}()
