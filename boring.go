10c56504e9d792d22b916e165c9ca6ce7355dba6
Scott Lowrey <sdlowrey@gmail.com>
2017-02-04 14:09:51 -0500

    goroutine communication

--- boring.go
+++ boring.go
@@ -6,16 +6,20 @@ import (
 	"math/rand"
 )
 
-func boring(msg string) {
+func boring(msg string, c chan string) {
+	rand.Seed(rand.Int63())
 	for i := 0; ; i++ {
-		fmt.Println(msg, i)
+		c <- fmt.Sprintf("%s %d", msg, i)
 		time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
 	}
 }
 
 func main() {
-	go boring("so boring")
+	c := make(chan string)
+	go boring("so boring", c)
 	fmt.Println("I'm listening.")
-	time.Sleep(time.Second * 2)
+	for i := 0; i < 5; i++ {
+		fmt.Printf("You said %s\n", <-c)
+	}
 	fmt.Println("You're boring. Bye!")
 }
