b715153eb5a769c2caafa3176d033480e623d2c2
Scott Lowrey <sdlowrey@gmail.com>
2017-02-04 14:43:34 -0500

    convert the function to a generator

--- boring.go
+++ boring.go
@@ -2,21 +2,24 @@ package main
 
 import (
 	"fmt"
-	"time"
 	"math/rand"
+	"time"
 )
 
-func boring(msg string, c chan string) {
+func boring(msg string) chan string {
+	c := make(chan string)
 	rand.Seed(rand.Int63())
-	for i := 0; ; i++ {
-		c <- fmt.Sprintf("%s %d", msg, i)
-		time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
-	}
+	go func() {
+		for i := 0; ; i++ {
+			c <- fmt.Sprintf("%s %d", msg, i)
+			time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
+		}
+	}()
+	return c
 }
 
 func main() {
-	c := make(chan string)
-	go boring("so boring", c)
+	c := boring("so boring")
 	fmt.Println("I'm listening.")
 	for i := 0; i < 5; i++ {
 		fmt.Printf("You said %s\n", <-c)
