5570c456c18cacf1a8f9e729910131bc0c0f54fc
Scott Lowrey <sdlowrey@gmail.com>
2017-02-04 12:08:51 -0500

    randomize the boringness interval

--- boring.go
+++ boring.go
@@ -3,12 +3,13 @@ package main
 import (
 	"fmt"
 	"time"
+	"math/rand"
 )
 
 func boring(msg string) {
 	for i := 0; ; i++ {
 		fmt.Println(msg, i)
-		time.Sleep(time.Second)
+		time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
 	}
 }
 
